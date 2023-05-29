276b034026e372a8612bc06ca129b6e230e4d76b
Diego <diegolopezreduello@gmail.com>
2023-05-30 01:15:10 +0200

    Teleportation and sound

--- src/main/java/net/deinigu/deltablademod/item/custom/DeltaBladeItem.java
+++ src/main/java/net/deinigu/deltablademod/item/custom/DeltaBladeItem.java
@@ -1,15 +1,28 @@
 package net.deinigu.deltablademod.item.custom;
 
+import com.mojang.math.Vector3d;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class DeltaBladeItem extends SwordItem {
 
@@ -27,6 +40,39 @@ public class DeltaBladeItem extends SwordItem {
 
         return super.hurtEnemy(pStack,pTarget,pAttacker);
     }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand)
+    {
+        BlockHitResult ray = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
+        BlockPos lookPos = ray.getBlockPos().relative(ray.getDirection());
+        player.setPos(lookPos.getX(), lookPos.getY(), lookPos.getZ());
+        level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
+        player.getCooldowns().addCooldown(this, 60);
+        player.fallDistance = 0F;
+        return super.use(level, player, hand);
+    }
+
+    protected static BlockHitResult rayTrace(Level level, Player player, ClipContext.Fluid fluidMode) {
+        double range = 15;
+
+        float f = player.getXRot();
+        float f1 = player.getYRot();
+        Vec3 vector3d = player.getEyePosition(1.0F);
+        float f2 = Mth.cos(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
+        float f3 = Mth.sin(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
+        float f4 = -Mth.cos(-f * ((float)Math.PI / 180F));
+        float f5 = Mth.sin(-f * ((float)Math.PI / 180F));
+        float f6 = f3 * f4;
+        float f7 = f2 * f4;
+        Vec3 vector3d1 = vector3d.add((double)f6 * range, (double)f5 * range, (double)f7 * range);
+
+
+
+        return level.clip(new ClipContext(vector3d, vector3d1, ClipContext.Block.OUTLINE, fluidMode, player));
+    }
+
+
 
 
 }
