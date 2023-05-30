c79e89fe2de344a0b74a0b9d10c8ec6fd9820a3b
Diego <diegolopezreduello@gmail.com>
2023-05-30 23:17:14 +0200

    Change speed and erased healing

--- src/main/java/net/deinigu/deltablademod/item/custom/DeltaBladeItem.java
+++ src/main/java/net/deinigu/deltablademod/item/custom/DeltaBladeItem.java
@@ -1,6 +1,5 @@
 package net.deinigu.deltablademod.item.custom;
 
-import com.mojang.math.Vector3d;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
@@ -8,10 +7,8 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -21,7 +18,6 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
 public class DeltaBladeItem extends SwordItem {
@@ -36,7 +32,6 @@ public class DeltaBladeItem extends SwordItem {
         pTarget.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 200), pAttacker);
         pTarget.addEffect(new MobEffectInstance(MobEffects.POISON, 200), pAttacker);
         pTarget.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 200), pAttacker);
-        pAttacker.addEffect(new MobEffectInstance(MobEffects.HEAL, 20), pAttacker);
 
         return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
