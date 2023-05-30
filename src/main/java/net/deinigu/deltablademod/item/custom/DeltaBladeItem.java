4b99f0081593130f9174174017ddecf4c0638e29
Diego <diegolopezreduello@gmail.com>
2023-05-30 20:15:10 +0200

    Particless

--- src/main/java/net/deinigu/deltablademod/item/custom/DeltaBladeItem.java
+++ src/main/java/net/deinigu/deltablademod/item/custom/DeltaBladeItem.java
@@ -27,27 +27,28 @@ import net.minecraft.world.phys.Vec3;
 public class DeltaBladeItem extends SwordItem {
 
     public DeltaBladeItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Item.Properties properties) {
-        super(pTier,pAttackDamageModifier,pAttackSpeedModifier,properties);
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, properties);
     }
 
     @Override
     public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
-        pTarget.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,200),pAttacker);
-        pTarget.addEffect(new MobEffectInstance(MobEffects.BLINDNESS,200),pAttacker);
-        pTarget.addEffect(new MobEffectInstance(MobEffects.POISON,200),pAttacker);
-        pTarget.addEffect(new MobEffectInstance(MobEffects.CONFUSION,200),pAttacker);
-        pAttacker.addEffect(new MobEffectInstance(MobEffects.HEAL,20),pAttacker);
+        pTarget.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 200), pAttacker);
+        pTarget.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 200), pAttacker);
+        pTarget.addEffect(new MobEffectInstance(MobEffects.POISON, 200), pAttacker);
+        pTarget.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 200), pAttacker);
+        pAttacker.addEffect(new MobEffectInstance(MobEffects.HEAL, 20), pAttacker);
 
-        return super.hurtEnemy(pStack,pTarget,pAttacker);
+        return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand)
-    {
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         BlockHitResult ray = rayTrace(level, player, ClipContext.Fluid.NONE);
         BlockPos lookPos = ray.getBlockPos().relative(ray.getDirection());
+        summonParticle(level, player);
         player.setPos(lookPos.getX(), lookPos.getY(), lookPos.getZ());
         level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
+        summonParticle(level, player);
         player.getCooldowns().addCooldown(this, 60);
         player.fallDistance = 0F;
         return super.use(level, player, hand);
@@ -59,20 +60,25 @@ public class DeltaBladeItem extends SwordItem {
         float f = player.getXRot();
         float f1 = player.getYRot();
         Vec3 vector3d = player.getEyePosition(1.0F);
-        float f2 = Mth.cos(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
-        float f3 = Mth.sin(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
-        float f4 = -Mth.cos(-f * ((float)Math.PI / 180F));
-        float f5 = Mth.sin(-f * ((float)Math.PI / 180F));
+        float f2 = Mth.cos(-f1 * ((float) Math.PI / 180F) - (float) Math.PI);
+        float f3 = Mth.sin(-f1 * ((float) Math.PI / 180F) - (float) Math.PI);
+        float f4 = -Mth.cos(-f * ((float) Math.PI / 180F));
+        float f5 = Mth.sin(-f * ((float) Math.PI / 180F));
         float f6 = f3 * f4;
         float f7 = f2 * f4;
-        Vec3 vector3d1 = vector3d.add((double)f6 * range, (double)f5 * range, (double)f7 * range);
-
+        Vec3 vector3d1 = vector3d.add((double) f6 * range, (double) f5 * range, (double) f7 * range);
 
 
         return level.clip(new ClipContext(vector3d, vector3d1, ClipContext.Block.OUTLINE, fluidMode, player));
     }
 
-
+    private void summonParticle(Level level, Player player) {
+        for (int i = 0; i < 360; i++) {
+            if (i % 10 == 0) {
+                level.addParticle(ParticleTypes.PORTAL, player.getX() + 0.5d, player.getY() + 1, player.getZ() + 0.5d, Math.cos(i) * 0.25d, 0.15d, Math.sin(i) * 0.25d);
+            }
+        }
+    }
 
 
 }
